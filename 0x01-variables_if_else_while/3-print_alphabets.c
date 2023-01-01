e40ade85ea613dceada0e9009aa953ef8541cf50
MrKwebz <mrkwebz@gmail.com>
2023-01-01 07:17:27 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+/**
+ *
+ *  * main - prints the alphabet in lowercase letters, and then in uppercase letters
+ *
+ *  * followed by a new line
+ *
+ *  * Return: Always 0 (Success)
+ *
+ *  */
+
+int main(void)
+
+{
+
+		int ch;
+
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+		
+		putchar(ch);
+
+		for (ch = 'A'; ch <= 'Z'; ch++)
+
+		putchar(ch);
+	
+		putchar('\n');
+
+		return (0);
+
+}
